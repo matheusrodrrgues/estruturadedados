c4c974bac69ec4b7026a441005f06f6b8ca41cc8
matheusrodrrgues <sirmathx@hotmail.com>
2025-04-10 14:09:48 -0300

    up aula

--- teste.c
+++ teste.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void troca(int *a, int *b){
+    int aux;
+    aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
 int main(void){
     int x, y, aux;
     scanf("%d %d", &x, &y);
-    aux = x;
-    x = y;
-    y = aux;
+    troca(&x, &y);
     printf("%d %d\n", x, y);
     return 0;
 }
